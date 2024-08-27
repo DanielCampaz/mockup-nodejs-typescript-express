479942343bc1231b09e996f88cf878d905188e7b
Daniel Steven Campaz Ocoro <86497840+DanielCampaz@users.noreply.github.com>
2024-08-26 19:06:41 -0500

    Injecion de dependencias; nodemon congifuracion

--- src/root.ts
+++ src/root.ts
@@ -1,7 +1,18 @@
+import { Singleton } from "./inject";
+import { ConsoleLogger } from "./loggers/ConsoleLoger";
 import { main } from "./main";
+import { UserService } from "./services/user.service";
 
 async function root() {
+  await importObjects();
   main();
 }
 
 root();
+
+async function importObjects() {
+  if (Singleton !== undefined) {
+    Singleton(ConsoleLogger, "ConsoleLogger");
+    Singleton(UserService);
+  }
+}
